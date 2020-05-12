a616f0877435a96c4a04e1c7201b4fad9ae51b76
evan <aspaasevan@gmail.com>
2020-05-12 13:14:11 -0700

    migration for car-dealer created

--- migrations/20200512125900_car-dealer.js
+++ migrations/20200512125900_car-dealer.js
@@ -1,8 +1,14 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("cars", tbl => {
+    tbl.increments();
+
+    tbl.string('VIN', 17).notNullable().unique();
+    tbl.string('make', 20).notNullable();
+    tbl.string('model', 20).notNullable();
+    tbl.decimal('mileage', .2).notNullable();
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('cars');
 };
